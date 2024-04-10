6c46aa541d5101dcdafc219605de6e9497bbbb29
Reina <panyw513@gmail.com>
2024-04-10 18:50:58 +0800

    feat(ts): 利用函式設定產品是否顯示

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,11 @@ const product = [
   new Product(2, "金魚都能懂的 CSS 必學屬性", "博碩文化", 500, true, new Date(2024, 3, 10)),
 ];
 
+setProductDisplay(product[1], false);
+
 console.log(product);
 console.table(product);
+
+function setProductDisplay(product: Product, isShow: boolean) {
+  product.isShow = isShow;
+}
