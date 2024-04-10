cf79fe8c885cc2609025c20e0bf2b56f376da625
Reina <panyw513@gmail.com>
2024-04-10 17:24:41 +0800

    feat(ts): 利用變數紀錄產品資訊

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+let productId: number;
+productId = 1;
+console.log("productId Id = ", productId);
+
+const productName: string = "圖像 Angular 開發入門 第二版";
+console.log("Product Name = ", productName);
+
+const isShow = true;
+console.log("Is Show =", isShow);
+
+let creatDate = undefined;
