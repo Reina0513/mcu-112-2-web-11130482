f18d8a286d8585363a142c9a0b25d5352a10e2da
Reina <panyw513@gmail.com>
2024-04-10 17:30:49 +0800

    feat(ts): 利用 undefriend 變數宣告產品建檔日期

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 let productId: number;
 productId = 1;
-console.log("productId Id = ", productId);
+console.log("Product Id = ", productId);
 
 const productName: string = "圖像 Angular 開發入門 第二版";
 console.log("Product Name = ", productName);
 
 const isShow = true;
-console.log("Is Show =", isShow);
+console.log("Is Show = ", isShow);
 
 let creatDate = undefined;
+console.log("Create Date = ", creatDate);
