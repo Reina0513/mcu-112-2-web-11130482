ef80e4cc4de34807bc1fe539a6590f047c9876a6
Reina <panyw513@gmail.com>
2024-04-10 18:31:51 +0800

    feat(ts): 利用類別定義產品資料

--- src/index.ts
+++ src/index.ts
@@ -8,20 +8,5 @@ const product = {
   modifyDate: undefined,
 };
 
-let productId: number;
-productId = 1;
-console.log("Product Id = ", productId);
-
-const productName: string = "圖像 Angular 開發入門 第二版";
-console.log("Product Name = ", productName);
-
-const isShow = true;
-console.log("Is Show = ", isShow);
-
-let createDate = undefined;
-console.log("Create Date = ", createDate);
-
-let modifyDate: Date | undefined;
-console.log("Modify Date = ", modifyDate);
-
-let type: "none" | "doing" | "finish" = "doing";
+console.log(product);
+console.table(product);
