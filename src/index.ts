363000cedc058e4437972c5e0a29fd11fca7863d
Reina <panyw513@gmail.com>
2024-04-10 18:00:25 +0800

    feat(ts): 利用物件紀錄產品資料

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+const product = {
+  id: 1,
+  name: "圖像 Angular 開發入門 第二版",
+  company: "博碩文化",
+  price: 500,
+  isShow: true,
+  createDate: new Date(2024, 3, 10),
+  modifyDate: undefined,
+};
+
 let productId: number;
 productId = 1;
 console.log("Product Id = ", productId);
@@ -8,8 +18,8 @@ console.log("Product Name = ", productName);
 const isShow = true;
 console.log("Is Show = ", isShow);
 
-let creatDate = undefined;
-console.log("Create Date = ", creatDate);
+let createDate = undefined;
+console.log("Create Date = ", createDate);
 
 let modifyDate: Date | undefined;
 console.log("Modify Date = ", modifyDate);
