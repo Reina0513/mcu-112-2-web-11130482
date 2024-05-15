8c7728005456591e454501a562f10dc1a2606547
Reina <panyw513@gmail.com>
2024-05-15 22:32:54 +0800

    feat(product): 實作資料新增與刪除後重新取得資料

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, delay, of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Product } from '../model/product';
 
 @Injectable({
@@ -55,7 +55,7 @@ export class ProductService {
   }
 
   getList(): Observable<Product[]> {
-    return of(this._data).pipe(delay(2000));
+    return of(this._data);
   }
 
   add(product: Product): Observable<Product> {
