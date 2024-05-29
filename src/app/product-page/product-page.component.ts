97c3b63e0a252dfaa75a37b4b47e93799a6d259e
Reina <panyw513@gmail.com>
2024-05-29 21:13:48 +0800

    feat(product): 查詢列表單設定

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,5 +1,6 @@
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subject, startWith, switchMap } from 'rxjs';
 import { Product } from '../model/product';
@@ -9,7 +10,7 @@ import { ProductService } from '../services/product.service';
 @Component({
   selector: 'app-product-page',
   standalone: true,
-  imports: [AsyncPipe, ProductCardListComponent],
+  imports: [AsyncPipe, JsonPipe, ReactiveFormsModule, ProductCardListComponent],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.css',
 })
@@ -19,6 +20,8 @@ export class ProductPageComponent {
   private productService = inject(ProductService);
 
   private readonly refresh$ = new Subject<void>();
+
+  protected readonly formControl = new FormControl<string | undefined>(undefined, { nonNullable: true });
 
   readonly products$ = this.refresh$.pipe(
     startWith(undefined),
