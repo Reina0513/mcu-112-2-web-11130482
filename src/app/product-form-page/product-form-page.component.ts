acd248130f8a22250dad96584f3755233dc5edbb
Reina <panyw513@gmail.com>
2024-06-05 23:52:25 +0800

    feat(product-form): 新增產品資料作業

--- src/app/product-form-page/product-form-page.component.ts
+++ src/app/product-form-page/product-form-page.component.ts
@@ -1,10 +1,11 @@
 import { JsonPipe } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { ActivatedRoute, Data } from '@angular/router';
-import { map } from 'rxjs';
+import { ActivatedRoute, Data, Router } from '@angular/router';
+import { filter, map, tap } from 'rxjs';
 import { IProductForm } from '../interface/product-form.interface';
 import { Product } from '../model/product';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-form-page',
@@ -22,8 +23,12 @@ export class ProductFormPageComponent implements OnInit {
     authors: new FormArray<FormControl<string | null>>([]),
     company: new FormControl<string | null>(null, { validators: [Validators.required] }),
     isShow: new FormControl<boolean>(false, { nonNullable: true }),
-    price: new FormControl<number | null>(null, { validators: [Validators.required, Validators.pattern('^[0-9]$')] }),
+    price: new FormControl<number | null>(null, { validators: [Validators.required] }),
   });
+
+  get id(): FormControl<number | null> {
+    return this.form.get('id') as FormControl<number | null>;
+  }
 
   get name(): FormControl<string | null> {
     return this.form.get('name') as FormControl<string | null>;
@@ -37,14 +42,26 @@ export class ProductFormPageComponent implements OnInit {
     return this.form.get('company') as FormControl<string | null>;
   }
 
+  get isShow(): FormControl<boolean> {
+    return this.form.get('isShow') as FormControl<boolean>;
+  }
+
   get price(): FormControl<number | null> {
     return this.form.get('price') as FormControl<number | null>;
   }
 
-  product!: Product;
+  private readonly router = inject(Router);
+
+  private readonly productService = inject(ProductService);
 
   ngOnInit(): void {
-    this.route.data.pipe(map(({ product }: Data) => product)).subscribe((product) => (this.product = product));
+    this.route.data
+      .pipe(
+        map(({ product }: Data) => product as Product),
+        filter((product) => !!product),
+        tap(({ authors }) => this.onAddAuthors(authors.length))
+      )
+      .subscribe((product) => this.form.patchValue(product));
   }
 
   onAddAuthors(count = 1): void {
@@ -52,5 +69,24 @@ export class ProductFormPageComponent implements OnInit {
       const formControl = new FormControl<string | null>(null, { validators: [Validators.required] });
       this.authors.push(formControl);
     }
+  }
+
+  onSave(): void {
+    const formData = new Product({
+      id: this.id.value ?? undefined,
+      name: this.name.value!,
+      authors: this.authors.value.map((author) => author!),
+      company: this.company.value!,
+      isShow: this.isShow.value,
+      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date(),
+      price: this.price.value!,
+    });
+    const action$ = this.id.value ? this.productService.update(formData) : this.productService.add(formData);
+    action$.subscribe(() => this.router.navigate(['products']));
+  }
+
+  onCancel(): void {
+    this.router.navigate(['products']);
   }
 }
