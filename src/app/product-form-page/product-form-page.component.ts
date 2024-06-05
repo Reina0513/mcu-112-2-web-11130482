d510bce69b1bc441957a7606a3c50b939b7834a1
Reina <panyw513@gmail.com>
2024-06-05 23:45:03 +0800

    feat(product-form): 產品表單驗證實作

--- src/app/product-form-page/product-form-page.component.ts
+++ src/app/product-form-page/product-form-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Data } from '@angular/router';
 import { map } from 'rxjs';
 import { IProductForm } from '../interface/product-form.interface';
@@ -18,15 +18,27 @@ export class ProductFormPageComponent implements OnInit {
 
   form = new FormGroup<IProductForm>({
     id: new FormControl<number | null>(null),
-    name: new FormControl<string | null>(null),
+    name: new FormControl<string | null>(null, { validators: [Validators.required] }),
     authors: new FormArray<FormControl<string | null>>([]),
-    company: new FormControl<string | null>(null),
+    company: new FormControl<string | null>(null, { validators: [Validators.required] }),
     isShow: new FormControl<boolean>(false, { nonNullable: true }),
-    price: new FormControl<number | null>(null),
+    price: new FormControl<number | null>(null, { validators: [Validators.required, Validators.pattern('^[0-9]$')] }),
   });
+
+  get name(): FormControl<string | null> {
+    return this.form.get('name') as FormControl<string | null>;
+  }
 
   get authors(): FormArray<FormControl<string | null>> {
     return this.form.get('authors') as FormArray<FormControl<string | null>>;
+  }
+
+  get company(): FormControl<string | null> {
+    return this.form.get('company') as FormControl<string | null>;
+  }
+
+  get price(): FormControl<number | null> {
+    return this.form.get('price') as FormControl<number | null>;
   }
 
   product!: Product;
@@ -35,8 +47,10 @@ export class ProductFormPageComponent implements OnInit {
     this.route.data.pipe(map(({ product }: Data) => product)).subscribe((product) => (this.product = product));
   }
 
-  onAddAuthors(): void {
-    const formControl = new FormControl<string | null>(null);
-    this.authors.push(formControl);
+  onAddAuthors(count = 1): void {
+    for (let i = 1; i <= count; i++) {
+      const formControl = new FormControl<string | null>(null, { validators: [Validators.required] });
+      this.authors.push(formControl);
+    }
   }
 }
